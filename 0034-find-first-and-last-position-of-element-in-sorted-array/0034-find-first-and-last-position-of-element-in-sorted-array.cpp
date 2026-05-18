fd35e74bfcf2dd183c7c18eb6ed2b829d2a66c19
Niranjan <u24ai107@aid.svnit.ac.in>
2026-05-18 10:37:27 +0530

    Time: 0 ms (100%), Space: 17.5 MB (53.07%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,40 +1,38 @@
 class Solution {
 public:
-    vector<int> searchRange(vector<int>& nums, int target) {
-        int start = 0, end = nums.size()-1,mid ;
-        int first = -1;
-        int last = -1 ;
-        while (start <= end ) {
-            mid =  start + (end - start)/2;
-            if (nums[mid] == target ) {
-                first = mid ;
-                end = mid - 1;
+    vector<int> searchRange(vector<int>& arr, int target) {
+        int n = arr.size();
+        int start = 0 , end = n - 1 , mid ;
+        int first = -1 ; 
+        int last = -1 ; 
+
+        while(start <= end ) {
+            mid = start + (end - start ) / 2 ;
+            if(arr[mid] == target) {
+                first = mid ; 
+                end = mid - 1 ;
             }
-            else if (nums[mid] < target ) {
-                start = mid +1 ;
-            }
-            else {
-                end = mid - 1;
-            }
+            else if (arr[mid] < target) start = mid + 1 ;
+            else end = mid - 1 ; 
+            
+           
         }
-        start = 0, end = nums.size()-1;
-        while (start <= end ) {
-            mid =  start + (end - start)/2;
-            if (nums[mid] == target ) {
-                last = mid ;
-                start  = mid + 1;
+        start = 0;
+        end = n - 1; 
+
+        while(start <= end ) {
+            mid = start + (end - start ) / 2 ;
+            if(arr[mid] == target) {
+                last = mid ; 
+                start  = mid + 1 ;
             }
-            else if (nums[mid] < target ) {
-                start = mid +1 ;
-            }
-            else {
-                end = mid - 1;
-            }
+            else if (arr[mid] < target) start = mid + 1 ;
+            else end = mid - 1 ; 
         }
-        vector<int>a(2);
-        a[0]= first ;
-        a[1]= last ;
-        return a ;
+        vector<int> a(2) ;
+        a[0] = first ;  
+        a[1] = last  ;
+        return a;
     }
     
 };
